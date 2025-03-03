bcc966f7caf7b045209d869657190c97b88765d0
Ayush Pandey <26849639+ayushwritescode@users.noreply.github.com>
2025-03-03 19:49:02 +0530

    Routing improvements

--- src/routes/privateRoute.tsx
+++ src/routes/privateRoute.tsx
@@ -1,18 +1,21 @@
 import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../store/hooks';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { JSX } from 'react';
 
 const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
 
   const {loading,isAuthenticated} = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch()
+  
 
+  if(!loading){
     if (isAuthenticated){
       return children; 
     }
     else{
       return <Navigate to="/login" replace />
     }
-  
+  }
 
 };
 
--- src/routes/publicRoute.tsx
+++ src/routes/publicRoute.tsx
@@ -2,14 +2,14 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAppSelector } from '../store/hooks';
 import { JSX } from 'react';
 
-const PUBLIC_ROUTES = ['/', '/login', '/signup'];
+const PRIVATE_ROUTES = ['/dashboard']
 
 const PublicRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const {loading,isAuthenticated} = useAppSelector((state) => state.auth);
   const location = useLocation();
 
   if(!loading){
-  if (isAuthenticated && PUBLIC_ROUTES.includes(location.pathname)) {
+  if (isAuthenticated && !PRIVATE_ROUTES.includes(location.pathname)) {
     return <Navigate to="/dashboard" replace />;
   }
   else{
