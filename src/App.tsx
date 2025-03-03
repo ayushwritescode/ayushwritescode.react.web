e64d441aba690da96c6c600bce06802de38c76da
Ayush Pandey <26849639+ayushwritescode@users.noreply.github.com>
2025-03-03 19:58:28 +0530

    Routing improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { ThreeDot } from 'react-loading-indicators';
-import { useAppDispatch, useAppSelector } from './store/hooks';
+import { useAppSelector } from './store/hooks';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { initSession } from './store/auth/authThunks';
 
 const App: React.FC = () => {
   const { loading } = useAppSelector((state) => state.auth);
-    const appDispatch = useAppDispatch();
 
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-
-  useEffect(()=>{
-    appDispatch(initSession());
-  },[])
 
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
+import { initSession } from './store/auth/authThunks';
 
 
+store.dispatch(initSession()).finally(() => {
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <App />
       <Toaster position="top-right"/>
     </Provider>
 );
+});
